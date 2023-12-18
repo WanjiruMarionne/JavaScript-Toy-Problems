fee8f421b56ab8241638a351e759afb4d1b183d0
Wanjiru Mburu <wanjirumarionne@gmail.com>
2023-12-18 14:43:40 +0300

    Completed Toys Problem

--- problemfiles/netsalarycalc.js
+++ problemfiles/netsalarycalc.js
@@ -1,6 +1,6 @@
 //Employees net salary calculator per individual
 
-function netSalary (basicSalary, benefits, paye, nhifDeductions, nssfDeductions, relief) {
+function netSalary (basicSalary, benefits, paye, nhifDeductions, nssfDeductions, personalRelief, housingLevy) {
     let grossSalary = basicSalary + benefits
     
 // computes NSSF Deductions
@@ -32,7 +32,7 @@ function netSalary (basicSalary, benefits, paye, nhifDeductions, nssfDeductions,
             return 0.1*taxBand1 + 0.25*(taxBand2-taxBand1) + 0.3*(taxBand3-taxBand2) + 0.325*(taxBand4-taxBand3) + 0.35*(taxablePay-taxBand4);
         }
 
-// computes NHIF Deductions. Appropriate rates upto 29999, the rest to be adjusted accordingly
+// computes NHIF Deductions
     let nhifDeductions;
         if (grossSalary<6000) {
             return 150;
@@ -48,9 +48,33 @@ function netSalary (basicSalary, benefits, paye, nhifDeductions, nssfDeductions,
             return 750;
         } else if (grossSalary<30000 && grossSalary>24999) {
             return 850;
-        } else if (grossSalary>30000) {
+        } else if (grossSalary<35000 && grossSalary>29999) {
+            return 900;
+        } else if (grossSalary<40000 && grossSalary>34999) {
+            return 950;
+        } else if (grossSalary<45000 && grossSalary>39999) {
+            return 1000;
+        } else if (grossSalary<50000 && grossSalary>44999) {
+            return 1100;
+        } else if (grossSalary<60000 && grossSalary>49999) {
             return 1200;
+        } else if (grossSalary<70000 && grossSalary>59999) {
+            return 1300;
+        } else if (grossSalary<80000 && grossSalary>69999) {
+            return 1400;
+        } else if (grossSalary<90000 && grossSalary>79999) {
+            return 1500;
+        } else if (grossSalary<100000 && grossSalary>89999) {
+            return 1600;
+        } else if (grossSalary>99999) {
+            return 1700;
         }
-const netSalary = grossSalary - nssfDeductions - paye - nhifDeductions + relief
+    
+//computes housing levy
+    let housingLevy = 0.015* grossSalary
+
+
+
+ const netSalary = grossSalary - nssfDeductions - paye - nhifDeductions - housingLevy + personalRelief
 }
  
